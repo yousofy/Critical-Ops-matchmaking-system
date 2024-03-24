f2f69e9bc929fa886e9268ca13aca373c9f33d01
Yousof <yousofy@students.cs.ubc.ca>
2024-03-23 19:26:02 -0700

    Phase3 v2

--- src/main/ui/GuiCriticalOpsGame.java
+++ src/main/ui/GuiCriticalOpsGame.java
@@ -1,6 +1,7 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionListener;
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -9,6 +10,8 @@ import model.Server;
 import persistence.JsonReader;
 import persistence.JsonWriter;
 
+// Description: Critical Ops Matchmaking system graphical based interface with the
+//              code being inspired by the recommended projects and java documentation
 public class GuiCriticalOpsGame {
     private static final String JSON_STORE = "./data/server.json";
     private Server sv;
@@ -16,10 +19,37 @@ public class GuiCriticalOpsGame {
     private JsonReader jsonReader;
     private JFrame frame;
 
+    // MODIFIES: this
+    // EFFECTS: runs the matchmaking application by displaying a splash image first
     public GuiCriticalOpsGame() {
+        splashScreen();
+        try {
+            Thread.sleep(2500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         runGame();
     }
 
+    // MODIFIES: this
+    // EFFECTS: displays a splash screen with the Critical Ops logo for 2.5 seconds
+    private void splashScreen() {
+        JFrame splashScreen = new JFrame();
+        splashScreen.setUndecorated(true);
+        splashScreen.setSize(540, 540);
+        splashScreen.setLocationRelativeTo(null);
+
+        ImageIcon imageIcon = new ImageIcon("data/splash image.jpg");
+        JLabel label = new JLabel(imageIcon);
+        splashScreen.add(label);
+
+        splashScreen.setVisible(true);
+
+        new Timer(2500, e -> splashScreen.dispose()).start();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: constructs application and creates the panel
     private void runGame() {
         sv = new Server();
         jsonWriter = new JsonWriter(JSON_STORE);
@@ -27,28 +57,54 @@ public class GuiCriticalOpsGame {
 
         frame = new JFrame("Critical Ops Matchmaking System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setSize(540, 140);
+        frame.setSize(540, 540);
 
         JPanel panel = new JPanel();
         frame.add(panel);
         placeComponents(panel);
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
     }
 
+    // MODIFIES: this
+    // EFFECTS: places all the necessary components on the panel
     private void placeComponents(JPanel panel) {
         panel.setLayout(null);
 
-        createButton(panel, "Create Player", 10, 10, e -> createPlayer());
-        createButton(panel, "View Players", 180, 10, e -> viewPlayers());
-        createButton(panel, "Queue Matchmaking", 10, 70, e -> queueMatchmaking());
-        createButton(panel, "Claim Rewards", 180, 70, e -> claimRewards());
-        createButton(panel, "View Match History", 350, 70, e -> viewMatchHistory());
-        createButton(panel, "Report Player", 350, 10, e -> reportPlayer());
-        createButton(panel, "View Skins", 10, 40, e -> viewSkins());
-        createButton(panel, "Save Server", 180, 40, e -> saveServer());
-        createButton(panel, "Load Server", 350, 40, e -> loadServer());
+        createButton(panel, "Create Player", 10, 140, e -> createPlayer());
+        createButton(panel, "View Players", 180, 140, e -> viewPlayers());
+        createButton(panel, "Queue Matchmaking", 10, 305, e -> matchmake());
+        createButton(panel, "Claim Rewards", 180, 305, e -> claimRewards());
+        createButton(panel, "View Match History", 350, 305, e -> viewHistory());
+        createButton(panel, "Report Player", 350, 140, e -> reportPlayer());
+        createButton(panel, "View Skins", 10, 470, e -> viewSkins());
+        createButton(panel, "Save Server", 180, 470, e -> saveServer());
+        createButton(panel, "Load Server", 350, 470, e -> loadServer());
+
+        createImage(panel, "data/profile icon.png", 10, 10);
+        createImage(panel, "data/players icon.jpg", 180, 10);
+        createImage(panel, "data/matchmaking icon.jpeg", 10, 175);
+        createImage(panel, "data/rewards icon.jpg", 180, 175);
+        createImage(panel, "data/history icon.jpeg", 350, 175);
+        createImage(panel, "data/report icon.jpeg", 350, 10);
+        createImage(panel, "data/gun icon.jpg", 10, 340);
+        createImage(panel, "data/save icon.jpg", 180, 340);
+        createImage(panel, "data/load icon.jpg", 350, 340);
     }
 
+    // MODIFIES: this
+    // EFFECTS: adds image to the panel with the given image path and position
+    private void createImage(JPanel panel, String imagePath, int x, int y) {
+        ImageIcon icon = new ImageIcon(imagePath);
+        Image scaledImage = icon.getImage().getScaledInstance(125, -1, Image.SCALE_SMOOTH);
+        icon = new ImageIcon(scaledImage);
+        JLabel label = new JLabel(icon);
+        label.setBounds(x,y,160, icon.getIconHeight());
+        panel.add(label);
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adds button to the panel with the given ActionListener, name and position
     private void createButton(JPanel panel, String text, int x, int y, ActionListener actionListener) {
         JButton button = new JButton(text);
         button.setBounds(x, y, 160, 25);
@@ -56,6 +112,8 @@ public class GuiCriticalOpsGame {
         button.addActionListener(actionListener);
     }
 
+    // MODIFIES: this
+    // EFFECTS: creates a player with the given name and game mode
     private void createPlayer() {
         String name = JOptionPane.showInputDialog(frame, "Enter player name:");
         if (name == null) {
@@ -75,6 +133,7 @@ public class GuiCriticalOpsGame {
         }
     }
 
+    // EFFECTS: displays all current players in the server
     private void viewPlayers() {
         String[] columnNames = {"Name", "Game Mode", "Rank", "Banned"};
 
@@ -99,7 +158,9 @@ public class GuiCriticalOpsGame {
         playersFrame.setVisible(true);
     }
 
-    private void queueMatchmaking() {
+    // MODIFIES: this
+    // EFFECTS: finds a match for the given player if possible
+    private void matchmake() {
         String name = JOptionPane.showInputDialog(frame, "Enter the name of the player you would like"
                 + " to start matchmaking for:");
         if (name == null || sv.findPlayer(name) == null) {
@@ -116,6 +177,8 @@ public class GuiCriticalOpsGame {
         }
     }
 
+    // MODIFIES: this
+    // EFFECTS: claims rewards for the given player if applicable
     private void claimRewards() {
         String name = JOptionPane.showInputDialog(frame, "Enter the name of the player you would "
                 + "like to claim rewards for:");
@@ -129,7 +192,8 @@ public class GuiCriticalOpsGame {
         }
     }
 
-    private void viewMatchHistory() {
+    // EFFECTS: displays the given player's match history
+    private void viewHistory() {
         String name = JOptionPane.showInputDialog(frame, "Enter the name of the player you"
                 + " would like to view the history of:");
         if (name == null) {
@@ -151,6 +215,8 @@ public class GuiCriticalOpsGame {
         }
     }
 
+    // MODIFIES: this
+    // EFFECTS: reports the player specified
     private void reportPlayer() {
         String name = JOptionPane.showInputDialog(frame, "Enter the name of the player you would like to report:");
         if (name == null) {
@@ -163,6 +229,7 @@ public class GuiCriticalOpsGame {
         }
     }
 
+    // EFFECTS: displays the given player's skins in their inventory
     private void viewSkins() {
         String name = JOptionPane.showInputDialog(frame, "Enter the name of the player you"
                 + " would like to view the inventory of:");
@@ -185,6 +252,7 @@ public class GuiCriticalOpsGame {
         }
     }
 
+    // EFFECTS: saves the server to file
     private void saveServer() {
         try {
             jsonWriter.open();
@@ -193,9 +261,11 @@ public class GuiCriticalOpsGame {
             JOptionPane.showMessageDialog(frame, "Saved server to " + JSON_STORE);
         } catch (FileNotFoundException e) {
             JOptionPane.showMessageDialog(frame, "Unable to write to file: " + JSON_STORE);
-        }
+        }   
     }
 
+    // MODIFIES: this
+    // EFFECTS: loads server from file
     private void loadServer() {
         try {
             sv = jsonReader.read();
@@ -205,5 +275,3 @@ public class GuiCriticalOpsGame {
         }
     }
 }
-
-
